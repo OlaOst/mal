2b83c311ab5dc559508916eb04433d0805bc228a
OlaOst <olaa81@gmail.com>
2017-10-22 12:05:22 +0200

    Add command line history support

--- source/app.d
+++ source/app.d
@@ -1,29 +1,42 @@
 module main;
 
+import std.conv : to;
+import std.stdio : readln, write, writeln;
+
+import deimos.linenoise;
+
 import ast;
 import env;
 import repl;
 
 
 void main()
-{
-  import std.stdio : readln, write, writeln;
-  
+{  
   auto env = new Env(null);
   env["+"] = new MalFunc(&builtinAdd);
   env["*"] = new MalFunc(&builtinMul);
+  
+  linenoiseHistorySetMaxLen(128);
   
   while (true)
   {
     try
     {
-      "user> ".write;
-      readln.rep(env).writeln;
+      auto line = linenoise("user> ");
+      auto input = line.to!string;
+      
+      if (input.length == 0)
+        continue;
+        
+      if (input == "exit")
+        break;
+      
+      line.linenoiseHistoryAdd();
+        
+      input.rep(env).writeln;
     }
     catch (Exception ex)
-    {
-      import std.conv : to;
-      
+    { 
       writeln("EH? ", ex);
       writeln(env.print);
     }
