425bffb063074dae269d4738acfbf7d7fca04ca6
OlaOst <olaa81@gmail.com>
2017-10-28 18:52:23 +0200

    Add empty? builtin function

--- source/malcore.d
+++ source/malcore.d
@@ -17,6 +17,18 @@ Env makeCoreEnv()
   env["print"] = new MalBuiltinFunction(&builtinPrint);
   env["list"] = new MalBuiltinFunction((arguments) => new MalList(arguments));
   env["list?"] = new MalBuiltinFunction((arguments) => (arguments.length > 0 && typeid(arguments[0]) == typeid(MalList)) ? new MalTrue() : new MalFalse());
+  env["empty?"] = new MalBuiltinFunction(function MalType(MalType[] arguments)
+  {
+    import std.exception : enforce;
+    
+    auto list = cast(MalList)arguments[0];
+    enforce(list !is null, "First parameter to empty? should be a list, not " ~ arguments[0].type);
+    
+    if (list.items.length == 0)
+      return new MalTrue();
+    else
+      return new MalFalse();
+  });
   
   return env;
 }
